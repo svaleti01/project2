48613df806195fd100c90f8275fd288ceedb5097
Kulanthaivel <539617@cognizant.com>
2020-07-26 19:52:35 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project2
+test project for openshift
---
+++ src/main/java/com/openshift/project2/Project2Application.java
@@ -0,0 +1,21 @@
+package com.openshift.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Project2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2Application.class, args);
+	}
+	
+	@RequestMapping(value="/getGreeting")
+	public String getGreeting() {
+		return ("Hello Openshift!! Here I come, Welcome Me :)");
+	}
+
+}
