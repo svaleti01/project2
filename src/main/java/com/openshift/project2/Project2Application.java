b2862bcc540d64b3398044057560638437cc3f41
Kulanthaivel <539617@cognizant.com>
2020-07-30 23:57:04 +0530

    added api call

--- src/main/java/com/openshift/project2/Project2Application.java
+++ src/main/java/com/openshift/project2/Project2Application.java
@@ -1,13 +1,18 @@
 package com.openshift.project2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class Project2Application {
+	
+	@Value("${SERVICE_URL}")
+    private String SERVICE_URL;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
@@ -15,7 +20,11 @@ public class Project2Application {
 	
 	@RequestMapping(value="")
 	public String getGreeting() {
-		return ("Hello Openshift!! Here I come, Welcome Me!! Thank you!)");
+		final String url = SERVICE_URL + "/getgreeting";
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String resp = restTemplate.getForObject(url, String.class);
+		return (resp +  " Openshift!! Here I come, Welcome Me!! Thank you!)");
 	}
 
 }
